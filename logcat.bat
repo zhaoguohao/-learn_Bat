9894e75cd36432f8b34a46f962cc8c8de49bc740
paul <zhaoguohao97@sina.com>
2019-08-02 20:29:48 +0800

    一个logcat的启动，一个多软件的启动，后续会贴出来logcat环境配置图文

---
+++ logcat.bat
@@ -0,0 +1,4 @@
+@echo off
+echo  不伤手的立白提醒你保护好眼镜
+adb logcat -s Unity
+pause
---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+start "Î¢ÐÅ" "D:\software\WeChat\WeChat.exe" &
+ping localhost -n 3
+start "TIM" "D:\software\Bin\QQScLauncher.exe" &
+ping localhost -n 3
+start "¶¤¶¤" "D:\software\DingDing\DingtalkLauncher.exe" &
+ping localhost -n 3
+start "Unity " "D:\Unity2018.2.1f1\Unity\Editor\Unity.exe" &
+ping localhost -n 3
+start "Unity " "C:\Users\9\AppData\Local\SourceTree\SourceTree.exe" 
+exit
